ac1961fcff6eddaf150e805d34e8402593df5c44
Levi Durfee <levi.durfee@gmail.com>
2022-05-28 08:29:23 -0400

    Compile app if there isn't a binary

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-go run cmd/client/main.go
+FILE=./main
+if [ ! -f "$FILE" ]; then
+    echo "Compiling..."
+    go build cmd/client/main.go
+fi
+
+./main
 npm run build
 npx wrangler pages publish build --commit-dirty=true
